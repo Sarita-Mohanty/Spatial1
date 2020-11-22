6e00650e9b926b43868261259509b4ff12438fce
Sarita-Mohanty <57366420+Sarita-Mohanty@users.noreply.github.com>
2020-11-21 23:47:09 -0500

    Importing libraries

--- Tensorflow_Example.py
+++ Tensorflow_Example.py
@@ -6,4 +6,7 @@ Created on Thu Nov 19 21:10:13 2020
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("C:/Users/arita/Documents/SpatialProject/Spatial1/Derived_C.csv")
 
